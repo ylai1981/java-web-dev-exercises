b82d80f951e0834cffd4834d132495fd08a6a49e
Jilar Beltran <jilarb@icloud.com>
2020-06-22 16:16:23 -0400

    modified files for studio1

--- src/org/launchcode/java/studios/areaofacircle/Area.java
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -1,4 +1,35 @@
 package org.launchcode.java.studios.areaofacircle;
 
+import java.util.Scanner;
+import java.lang.*;
+
 public class Area {
+    public static void main(String[] args){
+        double radius = 0;
+        double area;
+        boolean error = false;
+        Scanner input = new Scanner(System.in);
+
+        do{
+            System.out.println("Enter a radius: ");
+            try{
+                radius = input.nextDouble();
+            }
+            catch (Exception e) {
+                error = true;
+                break;
+            }
+        }while(radius < 0) ;
+
+        input.close();
+
+        if(error){
+            System.out.println("Please enter a number!");
+        }
+            area = Circle.getArea(radius);
+            System.out.println("The area of the circle with a radius of " + radius + " is: " + area);
+
+
+
+    }
 }
--- src/org/launchcode/java/studios/areaofacircle/Circle.java
+++ src/org/launchcode/java/studios/areaofacircle/Circle.java
@@ -1,4 +1,10 @@
 package org.launchcode.java.studios.areaofacircle;
 
 public class Circle {
+    public static Double getArea(Double radius) {
+        double area;
+
+        area = Math.PI * Math.pow(radius,2);
+        return area;
+    }
 }
