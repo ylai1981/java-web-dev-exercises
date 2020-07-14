b1a682788a643e160be34d4d701e9a421a6fd40b
Jilar Beltran <jilarb@icloud.com>
2020-07-14 15:47:04 -0500

    modified counting characters studio

---
+++ src/org/launchcode/java/studios/org/launchcode/java/studios/countingcharacters/CharacterCount.java
@@ -0,0 +1,32 @@
+package org.launchcode.java.studios.countingcharacters;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CharacterCount {
+    // Iterate over characters of a String
+    public static void main(String[] args) {
+        HashMap<Character, Integer> finalChar = new HashMap<>();
+        int charCount = 1;
+
+        String str = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. " +
+                "So if you move all the terms over to one side, " +
+                "you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. " +
+                "Once you’ve done that, it’s pretty straightforward from there.";
+
+        // convert String to char[] array
+        char[] charInString = str.toCharArray();
+
+        // iterate over charInString[] array using enhanced for loop
+        for (char charName : charInString) {
+
+            if (finalChar.containsKey(charName)) {
+                finalChar.put(charName, finalChar.get(charName) + 1);
+            } else {
+                finalChar.put(charName, 1);
+            }
+
+        }
+        System.out.println(finalChar);
+    }
+}
